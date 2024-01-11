b1d1003c3586333ed224f48e740cb9aa8efbdc20
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-01-11 20:02:31 +0100

    modifiche

---
+++ myScript.js
@@ -0,0 +1,4 @@
+let dataCorrente = new Date();
+let annoCorrente = dataCorrente.getFullYear();
+let copy = document.getElementById("copy");
+copy.innerHTML = `© ${annoCorrente} Geeks. Tutti i diritti sono riservati`;
